93a9f7bedfe64d5c9dc7ced277d9d9c244a1c4da
Syukron Rifail M <syukronrifai@gmail.com>
2018-05-27 07:19:23 +0000

    change to var

--- src/main/scala/ta/Constants.scala
+++ src/main/scala/ta/Constants.scala
@@ -8,7 +8,7 @@ object Constants {
   @inline var GRID_HEIGHT: Double = 5
   @inline final val DIMENSION = 2
 
-  @inline val N_OBJECTS = 100
+  @inline var N_OBJECTS = 100
   @inline final val N_POINTS = 100
 
   @inline val MIN_DATASPACE = 0
@@ -18,7 +18,7 @@ object Constants {
 
   @inline val PERCENT_DISTANCE = 0.5
 
-  @inline val TIME_EXPIRATION = 100
+  @inline var TIME_EXPIRATION = 100
 
   @inline val MIN_EDGE_ID = 0
   @inline val MAX_EDGE_ID = 21692

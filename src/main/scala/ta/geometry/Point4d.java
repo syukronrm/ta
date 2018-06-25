4a76e71e0e2398caab7c36a8dcc66180beafa023
Syukron Rifa'il M <syukronrifai@gmail.com>
2018-06-26 05:42:02 +0700

    belum selesei

---
+++ src/main/scala/ta/geometry/Point4d.java
@@ -0,0 +1,130 @@
+package ta.geometry;
+
+/*
+ * #%L
+ * Conversant RTree
+ * ~~
+ * Conversantmedia.com © 2016, Conversant, Inc. Conversant® is a trademark of Conversant, Inc.
+ * ~~
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ *
+ * Modifications copyright (C) 2018 <syukronrm>
+ */
+
+import collection.spatial.HyperPoint;
+import collection.spatial.HyperRect;
+import collection.spatial.RTree;
+import collection.spatial.RectBuilder;
+
+public final class Point4d implements HyperPoint {
+    public final static int X = 0;
+    public final static int Y = 1;
+    public final static int Z = 2;
+
+    public final double x, y, z, p;
+    public final int o;
+
+    public Point4d(final double x, final double y, final double z, final double p, final int o) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.p = p;
+        this.o = o;
+    }
+
+    public Point4d(final double x, final double y, final double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.p = 0;
+        this.o = 0;
+    }
+
+    @Override
+    public int getNDim() {
+        return 3;
+    }
+
+    @Override
+    public Double getCoord(final int d) {
+        if(d==X) {
+            return x;
+        } else if(d==Y) {
+            return y;
+        } else if(d==Z) {
+            return z;
+        } else {
+            throw new IllegalArgumentException("Invalid dimension");
+        }
+    }
+
+    @Override
+    public double distance(final HyperPoint p) {
+        final Point4d p2 = (Point4d)p;
+
+        final double dx = p2.x-x;
+        final double dy = p2.y-y;
+        final double dz = p2.z-z;
+        return Math.sqrt(dx*dx + dy*dy + dz*dz);
+    }
+
+    @Override
+    public double distance(final HyperPoint p, final int d) {
+        final Point4d p2 = (Point4d)p;
+        if(d == X) {
+            return Math.abs(p2.x - x);
+        } else if (d == Y) {
+            return Math.abs(p2.y - y);
+        } else if (d == Z) {
+            return Math.abs(p2.z - z);
+        } else {
+            throw new IllegalArgumentException("Invalid dimension");
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        final Point4d p = (Point4d)o;
+        return RTree.isEqual(x, p.x) &&
+                RTree.isEqual(y, p.y) &&
+                RTree.isEqual(z, p.z) &&
+                RTree.isEqual(this.p, p.p) &&
+                this.o == p.p;
+    }
+
+    @Override
+    public int hashCode() {
+        return Double.hashCode(x) ^
+                31*Double.hashCode(y) ^
+                31*31*Double.hashCode(z) ^
+                31*31*31*Double.hashCode(p) ^
+                31*31*Double.hashCode(o);
+    }
+
+    public final static class Builder implements RectBuilder<Point4d> {
+
+        @Override
+        public HyperRect getBBox(final Point4d point) {
+            return new Rect4d(point);
+        }
+
+        @Override
+        public HyperRect getMbr(final HyperPoint p1, final HyperPoint p2) {
+            return new Rect4d((Point4d)p1, (Point4d)p2);
+        }
+    }
+}
---
+++ src/main/scala/ta/geometry/Rect4d.java
@@ -0,0 +1,304 @@
+package ta.geometry;
+
+/*
+ * #%L
+ * Conversant RTree
+ * ~~
+ * Conversantmedia.com © 2016, Conversant, Inc. Conversant® is a trademark of Conversant, Inc.
+ * ~~
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ *
+ * Modifications copyright (C) 2018 <syukronrm>
+ */
+
+import collection.spatial.HyperPoint;
+import collection.spatial.HyperRect;
+import collection.spatial.RectBuilder;
+
+import java.util.List;
+
+public final class Rect4d implements HyperRect {
+    private Point3d min, max;
+
+    public Rect4d(List<Point3d> points) {
+        setPoints(points);
+    }
+
+    Rect4d(final Point3d p) {
+        min = new Point3d(p.x, p.y, p.z);
+        max = new Point3d(p.x, p.y, p.z);
+    }
+
+    public Rect4d(final double x1, final double y1, final double z1, final double x2, final double y2, final double z2) {
+        min = new Point3d(x1, y1, z1);
+        max = new Point3d(x2, y2, z2);
+    }
+
+    Rect4d(final Point3d point1, final Point3d point2) {
+
+        final double minx, maxx, miny, maxy, minz, maxz;
+
+        if(point1.x < point2.x) {
+            minx = point1.x;
+            maxx = point2.x;
+        } else {
+            minx = point2.x;
+            maxx = point1.x;
+        }
+
+        if(point1.y < point2.y) {
+            miny = point1.y;
+            maxy = point2.y;
+        } else {
+            miny = point2.y;
+            maxy = point1.y;
+        }
+
+        if(point1.z < point2.z) {
+            minz = point1.z;
+            maxz = point2.z;
+        } else {
+            minz = point2.z;
+            maxz = point1.z;
+        }
+
+        min = new Point3d(minx, miny, minz);
+        max = new Point3d(maxx, maxy, maxz);
+    }
+
+    @Override
+    public HyperRect getMbr(final HyperRect r) {
+        final Rect4d r2 = (Rect4d)r;
+        final double minX = Math.min(min.x, r2.min.x);
+        final double minY = Math.min(min.y, r2.min.y);
+        final double minZ = Math.min(min.z, r2.min.z);
+        final double maxX = Math.max(max.x, r2.max.x);
+        final double maxY = Math.max(max.y, r2.max.y);
+        final double maxZ = Math.max(max.z, r2.max.z);
+
+        return new Rect4d(minX, minY, minZ, maxX, maxY, maxZ);
+
+    }
+
+    @Override
+    public int getNDim() {
+        return 3;
+    }
+
+    @Override
+    public HyperPoint getCentroid() {
+        final double dx = min.x + (max.x - min.x)/2.0;
+        final double dy = min.y + (max.y - min.y)/2.0;
+        final double dz = min.z + (max.z - min.z)/2.0;
+
+        return new Point3d(dx, dy, dz);
+    }
+
+    @Override
+    public HyperPoint getMin() {
+        return min;
+    }
+
+    @Override
+    public HyperPoint getMax() {
+        return max;
+    }
+
+    @Override
+    public double getRange(final int d) {
+        if(d == 0) {
+            return max.x - min.x;
+        } else if(d == 1) {
+            return max.y - min.y;
+        } else if(d == 2) {
+            return max.z - min.z;
+        } else {
+            throw new IllegalArgumentException("Invalid dimension");
+        }
+    }
+
+    @Override
+    public boolean contains(final HyperRect r) {
+        final Rect4d r2 = (Rect4d)r;
+
+        return min.x <= r2.min.x &&
+                max.x >= r2.max.x &&
+                min.y <= r2.min.y &&
+                max.y >= r2.max.y &&
+                min.z <= r2.min.z &&
+                max.z >= r2.max.z;
+    }
+
+    @Override
+    public boolean intersects(final HyperRect r) {
+        final Rect4d r2 = (Rect4d)r;
+
+        return !(min.x > r2.max.x) &&
+                !(r2.min.x > max.x) &&
+                !(min.y > r2.max.y) &&
+                !(r2.min.y > max.y) &&
+                !(min.z > r2.max.z) &&
+                !(r2.min.z > max.z);
+    }
+
+    @Override
+    public double cost() {
+        final double dx = max.x - min.x;
+        final double dy = max.y - min.y;
+        final double dz = max.z - min.z;
+        return Math.abs(dx*dy*dz);
+    }
+
+    @Override
+    public double perimeter() {
+        double p = 0.0;
+        final int nD = this.getNDim();
+        for(int d = 0; d<nD; d++) {
+            p += 4.0 * this.getRange(d);
+        }
+        return p;
+    }
+
+    @Override
+    public HyperRect getPDR() {
+        final double minX = min.x;
+        final double minY = min.y;
+        final double minZ = min.z;
+        final double maxX = Double.MAX_VALUE;
+        final double maxY = Double.MAX_VALUE;
+        final double maxZ = Double.MAX_VALUE;
+
+        return new Rect4d(minX, minY, minZ, maxX, maxY, maxZ);
+    }
+
+    @Override
+    public HyperRect getDDR() {
+        final double minX = max.x;
+        final double minY = max.y;
+        final double minZ = max.z;
+        final double maxX = Double.MAX_VALUE;
+        final double maxY = Double.MAX_VALUE;
+        final double maxZ = Double.MAX_VALUE;
+
+        return new Rect4d(minX, minY, minZ, maxX, maxY, maxZ);
+    }
+
+    @Override
+    public HyperRect getPDD() {
+        final double minX = Double.MIN_VALUE;
+        final double minY = Double.MIN_VALUE;
+        final double minZ = Double.MIN_VALUE;
+        final double maxX = max.x;
+        final double maxY = max.y;
+        final double maxZ = max.z;
+
+        return new Rect4d(minX, minY, minZ, maxX, maxY, maxZ);
+    }
+
+    @Override
+    public HyperRect getDDD() {
+        final double minX = Double.MIN_VALUE;
+        final double minY = Double.MIN_VALUE;
+        final double minZ = Double.MIN_VALUE;
+        final double maxX = min.x;
+        final double maxY = min.y;
+        final double maxZ = min.z;
+
+        return new Rect4d(minX, minY, minZ, maxX, maxY, maxZ);
+    }
+
+    @Override
+    public void setPoints(List points) {
+        double minX, minY, minZ, maxX, maxY, maxZ;
+        minX = Double.MAX_VALUE;
+        minY = Double.MAX_VALUE;
+        minZ = Double.MAX_VALUE;
+        maxX = Double.MIN_VALUE;
+        maxY = Double.MIN_VALUE;
+        maxZ = Double.MIN_VALUE;
+
+        for (Object point : points) {
+            Point3d p = (Point3d) point;
+
+            if (p.x < minX) {
+                minX = p.x;
+            } else if (p.x > maxX) {
+                maxX = p.x;
+            }
+
+            if (p.y < minY) {
+                minY = p.y;
+            } else if (p.y > maxY) {
+                maxY = p.y;
+            }
+
+            if (p.z < minZ) {
+                minZ = p.z;
+            } else if (p.z > maxZ) {
+                maxZ = p.z;
+            }
+        }
+
+        min = new Point3d(minX, minY, minZ);
+        max = new Point3d(maxX, maxY, maxZ);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        final Rect4d rect3d = (Rect4d) o;
+
+        return min.equals(rect3d.min) &&
+                max.equals(rect3d.max);
+    }
+
+    @Override
+    public int hashCode() {
+        return min.hashCode() ^ 31*max.hashCode();
+    }
+
+    public String toString() {
+
+        return "(" +
+                Double.toString(min.x) +
+                ',' +
+                Double.toString(min.y) +
+                ',' +
+                Double.toString(min.z) +
+                ')' +
+                ' ' +
+                '(' +
+                Double.toString(max.x) +
+                ',' +
+                Double.toString(max.y) +
+                ',' +
+                Double.toString(max.z) +
+                ')';
+    }
+
+    public final static class Builder implements RectBuilder<Rect4d> {
+
+        @Override
+        public HyperRect getBBox(final Rect4d rect2D) {
+            return rect2D;
+        }
+
+        @Override
+        public HyperRect getMbr(final HyperPoint p1, final HyperPoint p2) {
+            return new Rect4d(p1.getCoord(0), p1.getCoord(1), p1.getCoord(2), p2.getCoord(0), p2.getCoord(1), p2.getCoord(2));
+        }
+    }
+}
